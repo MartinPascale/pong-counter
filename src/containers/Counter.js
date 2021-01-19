1e5e5928c75f400aa7e77fdd949a15417aad8cb8
MartinPascale <mpgpascale@gmail.com>
2021-01-19 16:54:54 -0300

    Add useReducer

--- src/containers/Counter.js
+++ src/containers/Counter.js
@@ -1,27 +1,35 @@
-import { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useReducer } from 'react';
+import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import PlayerCounter from 'components/PlayerCounter';
 
 import 'styles/containers/Counter.scss';
 
-const Counter = ({ names: { player1, player2 } }) => {
-  const history = useHistory();
-  const [player1Count, setplayer1Count] = useState(0);
-  const [player2Count, setplayer2Count] = useState(0);
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'player1':
+      return { ...state, player1Count: state.player1Count + 1 };
+    case 'player2':
+      return { ...state, player2Count: state.player2Count + 1 };
+    default:
+      throw new Error();
+  }
+};
 
-  useEffect(() => {
-    if (!(player1 && player1.trim() && player2 && player2.trim())) {
-      history.push('/');
-    }
-  }, []);
+const Counter = ({ names: { player1, player2 } }) => {
+  const initialState = { player1Count: 0, player2Count: 0 };
+
+  const [{ player1Count, player2Count }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
 
   const handleIncrement = (id) => {
     if (id === 0) {
-      setplayer1Count(player1Count + 1);
+      dispatch({ type: 'player1' });
     } else {
-      setplayer2Count(player2Count + 1);
+      dispatch({ type: 'player2' });
     }
   };
 
@@ -31,6 +39,10 @@ const Counter = ({ names: { player1, player2 } }) => {
   ];
 
   const isDraw = player1Count === player2Count;
+
+  if (!(player1 && player1.trim() && player2 && player2.trim())) {
+    return <Redirect to="/" />;
+  }
 
   return (
     <div className="counter">
@@ -58,8 +70,8 @@ const Counter = ({ names: { player1, player2 } }) => {
 };
 
 const namesShape = {
-  player1: PropTypes.string.isRequired,
-  player2: PropTypes.string.isRequired,
+  player1: PropTypes.string,
+  player2: PropTypes.string,
 };
 
 Counter.propTypes = {
